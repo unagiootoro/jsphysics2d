b7b913523fc31ffa7543f2dc9fc41539f09547a2
unagiootoro <ootoro838861@outlook.jp>
2023-11-25 15:07:25 +0900

    Update Rect

--- src/Rect.ts
+++ src/Rect.ts
@@ -3,6 +3,8 @@ import { Polygon } from "./Polygon";
 import { Vec2 } from "./Vec2";
 
 export class Rect extends Polygon {
+    private _size: Vec2;
+
     constructor(size: Vec2, opt: ICollisionShapeOption = {}) {
         const vertices = [
             new Vec2(-size.x / 2, -size.y / 2),
@@ -11,5 +13,16 @@ export class Rect extends Polygon {
             new Vec2(-size.x / 2, size.y / 2)
         ];
         super(vertices, opt);
+        this._size = size;
+    }
+
+    get size() { return this._size; }
+
+    clone(): Rect {
+        const rect = new Rect(this._size);
+        rect.position = this.position;
+        rect.angle = this.angle;
+        rect.anchor = this.anchor;
+        return rect;
     }
 }
